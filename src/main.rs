bb2b0cf5bddd1a293b936c105fb0453adef97c10
bkrusz <bkrusz@icloud.com>
2024-01-22 21:07:48 -0700

    Fixed mod decleration

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use std::io::{self};
+pub mod lexer;
 pub mod input_buffer;
-pub use crate::input_buffer::InputBuffer;
+pub use crate::lexer::Lexer;
 
 fn main() -> io::Result<()>{
     let mut file_path = String::new();
     io::stdin().read_line(&mut file_path).expect("Failed to read line");
     file_path.remove(file_path.len()-1);
-    let mut input_buffer = InputBuffer::build_buffer(file_path);
-    input_buffer.print_buffer();
     Ok(())
 }
