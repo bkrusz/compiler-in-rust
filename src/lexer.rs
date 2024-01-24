78352e5cda9cdc00e5178ed870de6f9a33cc6984
bkrusz <bkrusz@icloud.com>
2024-01-23 21:51:05 -0700

    Finished skip_space() and continued work on get_token_main()

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,22 +1,10 @@
 use core::fmt;
 pub use crate::input_buffer::InputBuffer;
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum TokenType {
-    END_OF_FILE,
-    MAIN,
-    PROC,
-    ENDPROC,
-    INPUT,
-    OUTPUT,
-    DO,
-    EQUAL,
-    NUM,
-    ID,
-    SEMICOLON,
-    PLUS,
-    MINUS,
-    MULT,
-    DIV,
+    END_OF_FILE, MAIN, PROC, ENDPROC,
+    INPUT, OUTPUT, DO, EQUAL, NUM, ID, 
+    SEMICOLON, PLUS, MINUS, MULT, DIV, 
     ERROR
 }
 
@@ -28,6 +16,7 @@ impl fmt::Display for TokenType {
 
 const KEYWORDS_COUNT: u8 = 7;
 
+#[derive(Clone)]
 struct Token {
     lexeme: String,
     token_type: TokenType,
@@ -44,19 +33,58 @@ pub struct Lexer {
     token_list: Vec<Token>,
     line_number: u8,
     index: u8,
-    temp_token: Token,
     input_buffer: InputBuffer
 }
 
 impl Lexer {
-    fn get_token_main() -> Token {
-        let mut c: char;
+    fn get_token_main(&mut self) -> Token {
+        let mut c: Option<char>;
+        let mut temp_token: Token = Token {
+            lexeme: String::from(""),
+            line_number: self.line_number,
+            token_type: TokenType::END_OF_FILE
+        };
 
-        
+        self.skip_space();
+        temp_token.lexeme = String::from("");
+        temp_token.line_number = self.line_number;
+        temp_token.token_type = TokenType::END_OF_FILE;
+
+        if !self.input_buffer.end_of_input() {
+            c = self.input_buffer.get_char();
+        } else {
+            return temp_token;
+        }
+
+        match c.unwrap_or_default() {
+            ';' => temp_token.token_type = TokenType::SEMICOLON,
+            '*' => temp_token.token_type = TokenType::MULT,
+            '/' => temp_token.token_type = TokenType::DIV,
+            '+' => temp_token.token_type = TokenType::PLUS,
+            '-' => temp_token.token_type = TokenType::MINUS,
+            '=' => temp_token.token_type = TokenType::EQUAL,
+            _ => if c.unwrap_or_default().is_ascii_digit() {
+                self.input_buffer.unget_char(c.unwrap_or_default())
+            }
+        }
+
+        return temp_token;
     }
 
     fn skip_space(&mut self) -> bool {
         let mut c = self.input_buffer.get_char();
-        let space_encountered = false;
+        let mut space_encountered = false;
+
+        if c.is_some_and(|c| c == '\n') { self.line_number += 1; }
+
+        while !self.input_buffer.end_of_input() && c.unwrap().is_whitespace() {
+            space_encountered = true;
+            c = self.input_buffer.get_char();
+            if c.is_some_and(|c| c == '\n') { self.line_number += 1; }
+        }
+
+        if !self.input_buffer.end_of_input() { self.input_buffer.unget_char(c.unwrap()); }
+        
+        return space_encountered;
     }
 }
