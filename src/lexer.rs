19d3453889787732057ebdcbb61329a322077792
Bailey Kruszewski <bkrusz@icloud.com>
2024-01-27 21:25:18 -0700

    Added funcionality to scan keywords and numbers

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,6 +1,7 @@
 use core::fmt;
+use std::str::FromStr;
 pub use crate::input_buffer::InputBuffer;
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq())]
 enum TokenType {
     END_OF_FILE, MAIN, PROC, ENDPROC,
     INPUT, OUTPUT, DO, EQUAL, NUM, ID, 
@@ -11,6 +12,23 @@ enum TokenType {
 impl fmt::Display for TokenType {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:?}", self)
+    }
+}
+
+impl FromStr for TokenType {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<TokenType, Self::Err> {
+        match s {
+            "MAIN" => Ok(TokenType::MAIN),
+            "PROC" => Ok(TokenType::PROC),
+            "ENDPROC" => Ok(TokenType::ENDPROC),
+            "INPUT" => Ok(TokenType::INPUT),
+            "OUTPUT" => Ok(TokenType::OUTPUT),
+            "DO" => Ok(TokenType::DO),
+            "EQUAL" => Ok(TokenType::EQUAL),
+            _ => Err(())
+        }
     }
 }
 
@@ -64,10 +82,9 @@ impl Lexer {
             '-' => temp_token.token_type = TokenType::MINUS,
             '=' => temp_token.token_type = TokenType::EQUAL,
             _ => if c.unwrap_or_default().is_ascii_digit() {
-                    self.input_buffer.unget_char(c.unwrap_or_default());
+                  self.input_buffer.unget_char(c.unwrap_or_default());
                     return self.scan_number();
                 } else if c.unwrap_or_default().is_ascii_alphabetic() {
-                    self.input_buffer.unget_char(c.unwrap_or_default());
                     return self.scan_id_or_keyword()
                 } else if self.input_buffer.end_of_input() {
                     temp_token.token_type = TokenType::END_OF_FILE;
@@ -80,15 +97,66 @@ impl Lexer {
     }
 
     fn scan_number(&mut self) -> Token {
+        let mut temp_token = Token {
+            lexeme: String::from(""),
+            line_number: self.line_number,
+            token_type: TokenType::ERROR
+        };
+        let mut c = self.input_buffer.get_char();
+        if c.unwrap_or_default().is_ascii_digit() {
+            if c.unwrap() == '0' {
+                temp_token.lexeme = String::from("0");
+            } else {
+                temp_token.lexeme = String::from("");
+                while !self.input_buffer.end_of_input() && c.unwrap().is_ascii_digit() {
+                    temp_token.lexeme += &String::from(c.unwrap());
+                    c = self.input_buffer.get_char();
+                }
+                if !self.input_buffer.end_of_input() {
+                    self.input_buffer.unget_char(c.unwrap());
+                }
+            }
 
+            temp_token.token_type = TokenType::NUM;
+            return temp_token;
+        } else {
+            if !self.input_buffer.end_of_input() {
+                self.input_buffer.unget_char(c.unwrap());
+            }
+
+            return temp_token;
+        }
     }
 
     fn scan_id_or_keyword(&mut self) -> Token {
+        let mut temp_token = Token {
+            lexeme: String::from(""),
+            line_number: self.line_number,
+            token_type: TokenType::ERROR
+        };
+        let c = self.input_buffer.get_char();
 
-    }
+        if c.unwrap_or_default().is_ascii_alphabetic() {
+            while !self.input_buffer.end_of_input() && c.unwrap_or_default().is_ascii() {
+                temp_token.lexeme += &String::from(c.unwrap());
+                self.input_buffer.get_char();
+            }
+            if !self.input_buffer.end_of_input() {
+                self.input_buffer.unget_char(c.unwrap());
+            }
+            if TokenType::from_str(&temp_token.lexeme).unwrap() != TokenType::ID {
+                temp_token.token_type = TokenType::into(temp_token.token_type);
+            } else {
+                temp_token.token_type = TokenType::ID;
+            }
+        }
+        else {
+            if !self.input_buffer.end_of_input() {
+                self.input_buffer.unget_char(c.unwrap());
+            }
+        }
 
-    fn find_keyword_index(&mut self) -> Token {
-
+        return temp_token;
     }
 
     fn get_token(&mut self) -> Token {
